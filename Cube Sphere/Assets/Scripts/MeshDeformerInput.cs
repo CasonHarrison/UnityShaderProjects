883aaec0d9805de974df28358b307550a245028e
CasonHarrison <122836195+CasonHarrison@users.noreply.github.com>
2024-12-04 13:12:38 -0500

    Custom Cube Sphere
    Includes multiple scenes show casing custom cube sphere with added colliders and mesh deformation.

---
+++ Cube Sphere/Assets/Scripts/MeshDeformerInput.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshDeformerInput : MonoBehaviour
+{
+    public float forceOffset = 0.1f;
+    public float force = 10f;
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            HandleInput();
+        }
+    }
+    void HandleInput()
+    {
+        Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(inputRay, out hit) )
+        {
+            MeshDeformer deformer = hit.collider.GetComponent<MeshDeformer>();
+            if (deformer)
+            {
+                Vector3 point = hit.point;
+                point += hit.normal * forceOffset;
+                deformer.AddDeformingForce(point, force);
+            }
+        }
+    }
+}
